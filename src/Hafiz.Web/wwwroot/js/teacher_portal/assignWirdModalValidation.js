19c5ac210bfe31ed0d678053592280a96cf63707
Abdulaziz Al-Hariri <75909460+abdulazeez761@users.noreply.github.com>
2026-03-02 07:14:54 +0300

    adding indicator for the required field

--- src/Hafiz.Web/wwwroot/js/teacher_portal/assignWirdModalValidation.js
+++ src/Hafiz.Web/wwwroot/js/teacher_portal/assignWirdModalValidation.js
@@ -1,3 +1,4 @@
+let form = document.getElementById('wirdAssignmentForm');
 let fromJuz = document.getElementById('FromJuz');
 let toJuz = document.getElementById('ToJuz');
 let fromSurah = document.getElementById('FromSurah');
@@ -7,14 +8,24 @@ let toAyah = document.getElementById('ToAyah');
 let assignmentType = document.getElementById('Type');
 //if the user picked from juz should enter to juz number
 fromJuz.addEventListener('change', function () {
-  if (fromJuz.value) toJuz.required = true;
-  else toJuz.required = false;
+  if (fromJuz.value) {
+    toJuz.required = true;
+    setRequired(toJuz, true);
+  } else {
+    toJuz.required = false;
+    setRequired(toJuz, false);
+  }
 });
 
 //if the user picked from surah should enter to surah number and only shows the suras after the from surah
 fromSurah.addEventListener('change', function () {
-  if (fromSurah.value) toSurah.required = true;
-  else toSurah.required = false;
+  if (fromSurah.value) {
+    toSurah.required = true;
+    setRequired(toSurah, true);
+  } else {
+    toSurah.required = false;
+    setRequired(toSurah, false);
+  }
   //show only the suras after the from surah
   let fromSurahNumber = parseInt(fromSurah.value) - 1;
   for (let i = 0; i < toSurah.options.length; i++) {
@@ -28,6 +39,21 @@ fromSurah.addEventListener('change', function () {
 });
 //if the user picked from ayah should enter to ayah number and only shows the ayahs after the from ayah
 fromAyah.addEventListener('change', function () {
-  if (fromAyah.value) toAyah.required = true;
-  else toAyah.required = false;
+  if (fromAyah.value) {
+    toAyah.required = true;
+    setRequired(toAyah, true);
+  } else {
+    toAyah.required = false;
+    setRequired(toAyah, false);
+  }
 });
+
+// do not allow to send an empty form
+
+function setRequired(field, isRequired) {
+  field.required = isRequired;
+  field
+    .closest('.form-group')
+    ?.querySelector('.form-label')
+    ?.classList.toggle('required', isRequired);
+}
