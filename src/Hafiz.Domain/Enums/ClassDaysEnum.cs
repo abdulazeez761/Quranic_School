324e586d00f0b000501e0c988f7177cfc03999ed
Abdulaziz Al-Hariri <75909460+abdulazeez761@users.noreply.github.com>
2026-06-07 03:11:28 +0300

    fixing the saturday issue

--- src/Hafiz.Domain/Enums/ClassDaysEnum.cs
+++ src/Hafiz.Domain/Enums/ClassDaysEnum.cs
@@ -2,12 +2,12 @@ namespace Hafiz.Models.enums
 {
     public enum ClassDaysEnum
     {
-        sunday,
-        monday,
-        tuesday,
-        wednesday,
-        thursday,
-        friday,
-        saturday,
+        sunday = 1,
+        monday = 2,
+        tuesday = 3,
+        wednesday = 4,
+        thursday = 5,
+        friday = 6,
+        saturday = 7,
     }
 }
